5be43c13f3c2d4d681a7984937a27de6fd634395
Connor Lane <connor.lane858@gmail.com>
2026-01-03 01:26:08 +0000

    cleanup

--- datasets/ABIDE/scripts/find_subjects.sh
+++ datasets/ABIDE/scripts/find_subjects.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm sourcedata/subject_list.txt 2>/dev/null
+rm metadata/subject_list.txt 2>/dev/null
 
 while read subdir; do
   dataset=$(echo $subdir | cut -d / -f 3)
   sub=$(echo $subdir | cut -d / -f 4)
   sub=${sub#sub-}
-  echo $dataset $sub >> sourcedata/subject_list.txt
-done < <(find sourcedata/RawDataBIDS -type d -name 'sub-*' | sort)
+  echo $dataset $sub >> metadata/subject_list.txt
+done < <(find data/RawDataBIDS -type d -name 'sub-*' | sort)
